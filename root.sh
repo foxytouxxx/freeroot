7fa72429ab9de86c3d0915227c2b1da02b39c9c1
foxytouxxx <143593532+foxytouxxx@users.noreply.github.com>
2024-02-10 20:14:36 +0100

    Create root.sh

---
+++ root.sh
@@ -0,0 +1,99 @@
+#!/bin/sh
+
+ROOTFS_DIR=$(pwd)
+export PATH=$PATH:~/.local/usr/bin
+max_retries=50
+timeout=1
+ARCH=$(uname -m)
+
+if [ "$ARCH" = "x86_64" ]; then
+  ARCH_ALT=amd64
+elif [ "$ARCH" = "aarch64" ]; then
+  ARCH_ALT=arm64
+else
+  printf "Unsupported CPU architecture: ${ARCH}"
+  exit 1
+fi
+
+if [ ! -e $ROOTFS_DIR/.installed ]; then
+  echo "#######################################################################################"
+  echo "#"
+  echo "#                                      Foxytoux INSTALLER"
+  echo "#"
+  echo "#                           Copyright (C) 2024 - 2023, RecodeStudios.Cloud"
+  echo "#"
+  echo "#"
+  echo "#######################################################################################"
+
+  read -p "Do you want to install Ubuntu? (YES/no): " install_ubuntu
+fi
+
+case $install_ubuntu in
+  [yY][eE][sS])
+    wget --tries=$max_retries --timeout=$timeout --no-hsts -O /tmp/rootfs.tar.gz \
+      "http://cdimage.ubuntu.com/ubuntu-base/releases/20.04/release/ubuntu-base-20.04.4-base-${ARCH_ALT}.tar.gz"
+    tar -xf /tmp/rootfs.tar.gz -C $ROOTFS_DIR
+    ;;
+  *)
+    echo "Skipping Ubuntu installation."
+    ;;
+esac
+
+if [ ! -e $ROOTFS_DIR/.installed ]; then
+  mkdir $ROOTFS_DIR/usr/local/bin -p
+  wget --tries=$max_retries --timeout=$timeout --no-hsts -O $ROOTFS_DIR/usr/local/bin/proot "https://raw.githubusercontent.com/dxomg/vpsfreepterovm/main/proot-${ARCH}"
+
+  while [ ! -s "$ROOTFS_DIR/usr/local/bin/proot" ]; do
+    rm $ROOTFS_DIR/usr/local/bin/proot -rf
+    wget --tries=$max_retries --timeout=$timeout --no-hsts -O $ROOTFS_DIR/usr/local/bin/proot "https://raw.githubusercontent.com/dxomg/vpsfreepterovm/main/proot-${ARCH}"
+
+    if [ -s "$ROOTFS_DIR/usr/local/bin/proot" ]; then
+      chmod 755 $ROOTFS_DIR/usr/local/bin/proot
+      break
+    fi
+
+    chmod 755 $ROOTFS_DIR/usr/local/bin/proot
+    sleep 1
+  done
+
+  chmod 755 $ROOTFS_DIR/usr/local/bin/proot
+fi
+
+if [ ! -e $ROOTFS_DIR/.installed ]; then
+  printf "nameserver 1.1.1.1\nnameserver 1.0.0.1" > ${ROOTFS_DIR}/etc/resolv.conf
+  rm -rf /tmp/rootfs.tar.xz /tmp/sbin
+  touch $ROOTFS_DIR/.installed
+fi
+
+BLACK='\e[0;30m'
+BOLD_BLACK='\e[1;30m'
+RED='\e[0;31m'
+BOLD_RED='\e[1;31m'
+GREEN='\e[0;32m'
+BOLD_GREEN='\e[1;32m'
+YELLOW='\e[0;33m'
+BOLD_YELLOW='\e[1;33m'
+BLUE='\e[0;34m'
+BOLD_BLUE='\e[1;34m'
+MAGENTA='\e[0;35m'
+BOLD_MAGENTA='\e[1;35m'
+CYAN='\e[0;36m'
+BOLD_CYAN='\e[1;36m'
+WHITE='\e[0;37m'
+BOLD_WHITE='\e[1;37m'
+RESET_COLOR='\e[0m'
+
+display_footer() {
+  echo -e "${WHITE}___________________________________________________${RESET_COLOR}"
+  echo -e ""
+  echo -e "           ${YELLOW}-----> Mission Completed ! <----${RESET_COLOR}"
+}
+
+clear
+display_header
+display_resources
+display_footer
+
+$ROOTFS_DIR/usr/local/bin/proot \
+  --rootfs="${ROOTFS_DIR}" \
+  -0 -w "/root" -b /dev -b /sys -b /proc -b /etc/resolv.conf --kill-on-exit
